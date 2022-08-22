844166705a7ea4b5936f3e1abedebf9b9a5cddc7
akram-webdev <akram.webdev@gmail.com>
2022-08-23 00:04:15 +0600

    connenct to data base and data loaded done

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-require('dotenv').config()
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+
+ 
+const uri = `mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASS}@cluster0.yx6bvjp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+
+ 
+ 
+
+async function run(){
+    try {
+        await client.connect();
+        const blogCollection = client.db('myBlog').collection('blog');
+ 
+
+        app.get('/blog', async (req, res) =>{
+            const query = {};
+            const cursor = blogCollection.find(query);
+            const blogs = await cursor.toArray()
+            res.send(blogs)
+         
+        })
+
+        
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(console.dir)
 
 
 
@@ -13,8 +53,7 @@ const app = express();
 
 
 //middleware
-app.use(cors())
-app.use(express.json())
+
 
 app.get('/', (req,res) =>{
     res.send('my blog server is running')
